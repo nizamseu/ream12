684ba23fd0561d623b03e830fd821a87754cb077
abu-yousuf1 <yousufadnan111@gmail.com>
2022-01-01 22:00:02 +0600

    card update

--- src/Components/Pages/Home/Category/Category.js
+++ src/Components/Pages/Home/Category/Category.js
@@ -1,10 +1,75 @@
 import React from 'react';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 const Category = () => {
     return (
-        <div>
+        <>
+            <Grid container spacing={2} >
+                <Grid item xs={6} md={3} >
+                    <Card sx={{ maxWidth: 300 }}>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            height=""
+                            image="https://www.lenovo.com/medias/lenovo-laptops-thinkpad-t-series-t14s-gen2-intel-hero.png?context=bWFzdGVyfHJvb3R8ODA4OTR8aW1hZ2UvcG5nfGgwMC9oOTQvMTE0NjgzNDU5MzM4NTQucG5nfGY2NGM1ODUyNmRlMDZiZjJiN2IzOWQ4Zjg2ZjEyOTNiY2QwNzUzMzY5M2M4ODk0YTgzNDEwOTRhMGI2OTQxNDU"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" sx={{ textAlign: 'center' }} component="div">
+                                Laptop
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={3} md={3} >
+                    <Card sx={{ maxWidth: 345, }}>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            height=""
+                            image="https://kddi-h.assetsadobe3.com/is/image/content/dam/au-com/mobile/mb_img_25.jpg?scl=1"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Mobile
+                            </Typography>
+                        </CardContent>
 
-        </div>
+                    </Card>
+                </Grid>
+                <Grid item xs={3} md={3}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            style={{ height: '80%' }}
+                            image="https://www.sony-asia.com/image/f1231591e8a1e2fe4e5b6ad229a756c9?fmt=pjpeg&wid=1014&hei=396&bgcolor=F1F5F9&bgc=F1F5F9"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Tv
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={3} md={3}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            alt="green iguana"
+                            style={{ height: '80%' }}
+                            image="https://expertvagabond.com/wp-content/uploads/best-travel-camera-guide-1-900x600.jpg.webp"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Camera
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+
+
+        </>
     );
 };
 
--- src/Components/Pages/Home/Products/Products.js
+++ src/Components/Pages/Home/Products/Products.js
@@ -1,10 +1,117 @@
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Rating, Typography } from '@mui/material';
 import React from 'react';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import './Products.css'
+
+const sofa = [
+    {
+        id: 1,
+        name: 'Rocking Chair',
+        image: 'https://image.freepik.com/free-psd/chair-pillow_176382-880.jpg',
+        price: 910,
+        rating: 4
+    },
+    {
+        id: 2,
+        name: 'Center Table Glass Metal',
+        image: 'https://image.freepik.com/free-psd/chair-pillow_176382-865.jpg',
+        price: 865,
+        rating: 3.5
+    },
+    {
+        id: 3,
+        name: 'Easy Chair',
+        image: 'https://img.freepik.com/free-psd/armchair-pillow_176382-886.jpg?size=338&ext=jpg',
+        price: 850,
+        rating: 5
+    },
+    {
+        id: 4,
+        name: 'Rocking Chair',
+        image: 'https://image.freepik.com/free-psd/chair-pillow_176382-880.jpg',
+        price: 910,
+        rating: 4
+    },
+    {
+        id: 5,
+        name: 'Center Table Glass Metal',
+        image: 'https://image.freepik.com/free-psd/chair-pillow_176382-865.jpg',
+        price: 865,
+        rating: 3.5
+    },
+    {
+        id: 6,
+        name: 'Easy Chair',
+        image: 'https://img.freepik.com/free-psd/armchair-pillow_176382-886.jpg?size=338&ext=jpg',
+        price: 850,
+        rating: 5
+    },
+    {
+        id: 7,
+        name: 'Rocking Chair',
+        image: 'https://image.freepik.com/free-psd/chair-pillow_176382-880.jpg',
+        price: 910,
+        rating: 4
+    },
+    {
+        id: 8,
+        name: 'Center Table Glass Metal',
+        image: 'https://image.freepik.com/free-psd/chair-pillow_176382-865.jpg',
+        price: 865,
+        rating: 3.5
+    },
+    {
+        id: 9,
+        name: 'Easy Chair',
+        image: 'https://img.freepik.com/free-psd/armchair-pillow_176382-886.jpg?size=338&ext=jpg',
+        price: 850,
+        rating: 5
+    }
+
+]
 
 const Products = () => {
     return (
-        <div>
+        <Container sx={{ marginTop: '50px' }}>
+            <Grid container spacing={{ xs: 2, md: 4 }} >
 
-        </div>
+                {
+                    sofa.map(item =>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Card className="card" sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    component="img"
+                                    alt="green iguana"
+                                    height="140"
+                                    image={item.image}
+                                />
+                                <CardContent>
+                                    {/* <Typography gutterBottom variant="h6" sx={{ color: 'text.secondary' }} component="div">
+                                        Lizard
+                                    </Typography> */}
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Lizard
+                                    </Typography>
+                                    <Typography gutterBottom variant="h5" sx={{ color: 'text.secondary' }} component="div">
+                                        $5464
+                                    </Typography>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        <Rating name="half-rating" defaultValue={2.5} precision={0.5} readOnly />
+                                    </Typography>
+                                </CardContent>
+                                <div className='addCard'>
+                                    <CardActions sx={{ justifyContent: 'center' }}>
+                                        <Button variant="contained"> <AddShoppingCartIcon sx={{ marginRight: '15px' }} />ADD TO CART</Button>
+                                    </CardActions>
+                                </div>
+                            </Card>
+                        </Grid>
+                    )
+                }
+
+            </Grid>
+        </Container>
+
     );
 };
 
