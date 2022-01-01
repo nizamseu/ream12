1970c1f567dc74ffed63c4b7e6b3a6d37bdc4e3f
abu-yousuf1 <yousufadnan111@gmail.com>
2022-01-01 12:13:36 +0600

    set-up project

---
+++ src/Components/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Category from '../Category/Category';
+
+const Home = () => {
+    return (
+        <div>
+            <Category />
+        </div>
+    );
+};
+
+export default Home;
