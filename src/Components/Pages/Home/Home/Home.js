0a39829d790354dc52598770a4ab18a69e0c5d6d
Faysal Akbar <faysalrj361@gmail.com>
2022-01-02 13:02:09 +0600

    login and registration form

--- src/Components/Pages/Home/Home/Home.js
+++ src/Components/Pages/Home/Home/Home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Login from "../../../Login/Login/Login";
-import Registration from "../../../Login/Registration/Registration";
 import Footer from "../../Shared/Footer/Footer";
 import Navbar from "../../Shared/Navbar/Navbar";
 import Products from "../Products/Products";
@@ -12,8 +10,6 @@ const Home = () => {
       {/* <Category /> */}
       <Products />
       <Footer></Footer>
-      <Login></Login>
-      <Registration></Registration>
     </div>
   );
 };
